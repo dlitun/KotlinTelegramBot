ee1598b1c51ad06ace9a0d420f935ac4504f86fa
dlitun <dlitun@ya.ru>
2025-11-18 20:39:41 +0300

    KTB-04: parse-file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,31 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0
+)
+
 fun main() {
-    val file = File("src/main/resources/words.txt")
-    val lines = file.readLines()
+    val lines = File("src/main/resources/words.txt").readLines()
+    val dictionary = mutableListOf<Word>()
 
     for (line in lines) {
-        println(line)
+        val parts = line.split("|")
+
+        val original = parts[0]
+        val translation = parts[1]
+
+        val correctAnswers =
+            if (parts.size == 3) parts[2].toInt()
+            else 0
+
+        val word = Word(original, translation, correctAnswers)
+        dictionary.add(word)
+    }
+
+    for (word in dictionary) {
+        println(word)
     }
 }
