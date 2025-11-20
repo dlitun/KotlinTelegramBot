22a38f5693ad97cd80633667d1116b2d8e4fefa1
dlitun <dlitun@ya.ru>
2025-11-20 13:36:10 +0300

    KTB-06: fun-statistics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,11 +14,9 @@ fun loadDictionary(): List<Word> {
 
     for (line in lines) {
         val parts = line.split("|")
-
         val original = parts[0]
         val translation = parts[1]
         val correctAnswers = parts.getOrNull(2)?.toInt() ?: 0
-
         val word = Word(original, translation, correctAnswers)
         dictionary.add(word)
     }
@@ -39,12 +37,29 @@ fun main() {
         val input = readln()
 
         when (input) {
-            "1" -> println("Вы выбрали: Учить слова")
-            "2" -> println("Вы выбрали: Статистика")
+            "1" -> {
+                println("Вы выбрали: Учить слова")
+            }
+
+            "2" -> {
+                val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }
+                val totalCount = dictionary.size
+                val learnedCount = learnedWords.size
+                val percent = if (totalCount > 0) {
+                    learnedCount * 100 / totalCount
+                } else {
+                    0
+                }
+
+                println("Выучено $learnedCount из $totalCount слов | $percent%")
+                println()
+            }
+
             "0" -> {
                 println("Выход из программы...")
                 break
             }
+
             else -> println("Введите число 1, 2 или 0")
         }
     }
