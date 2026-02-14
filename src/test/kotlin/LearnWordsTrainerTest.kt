c464156e359495959e8d197d3a4ffaaf931fd6a1
Dmitry Litun <dlitun@ya.ru>
2026-02-15 00:25:13 +0300

    KTB-25: unit-tests

---
+++ src/test/kotlin/LearnWordsTrainerTest.kt
@@ -0,0 +1,146 @@
+import data.DictionaryRepository
+import model.Statistics
+import org.junit.jupiter.api.Test
+import trainer.LearnWordsTrainer
+import java.io.File
+import kotlin.test.*
+
+class LearnWordsTrainerTest {
+
+    private fun createTempDictionaryFile(content: String): File =
+        File.createTempFile("dictionary_", ".txt").apply {
+            writeText(content.trimIndent())
+            deleteOnExit()
+        }
+
+    @Test
+    fun `test statistics with 4 words of 7`() {
+        val path = "src/test/resources/4_words_of_7.txt"
+        val repository = DictionaryRepository(path)
+        val trainer = LearnWordsTrainer(repository)
+
+        assertEquals(
+            Statistics(totalCount = 7, learnedCount = 4, percent = 57),
+            trainer.getStatistics()
+        )
+    }
+
+    @Test
+    fun `test statistics with corrupted file`() {
+        val file = createTempDictionaryFile(
+            """
+            good|хорошо|0
+            corrupted_line_without_separator
+            """
+        )
+
+        val repository = DictionaryRepository(file.absolutePath)
+        assertFailsWith<IndexOutOfBoundsException> {
+            LearnWordsTrainer(repository)
+        }
+    }
+
+    @Test
+    fun `test getNextQuestion() with 5 unlearned words`() {
+        val file = createTempDictionaryFile(
+            """
+            a|а|0
+            b|б|0
+            c|с|0
+            d|д|0
+            e|е|0
+            learned|выучено|3
+            """
+        )
+
+        val repository = DictionaryRepository(file.absolutePath)
+        val trainer = LearnWordsTrainer(repository)
+
+        val question = trainer.getNextQuestion()
+        assertNotNull(question)
+        assertTrue(question.options.isNotEmpty())
+    }
+
+    @Test
+    fun `test getNextQuestion() with 1 unleard word`() {
+        val file = createTempDictionaryFile(
+            """
+            only|только|0
+            learned1|выучено|3
+            learned2|выучено|3
+            """
+        )
+
+        val repository = DictionaryRepository(file.absolutePath)
+        val trainer = LearnWordsTrainer(repository)
+
+        val question = trainer.getNextQuestion()
+        assertNotNull(question)
+        assertTrue(question.options.size in 1..4)
+    }
+
+    @Test
+    fun `test getNextQuestion() with all words learned`() {
+        val file = createTempDictionaryFile(
+            """
+            a|а|3
+            b|б|3
+            c|с|3
+            """
+        )
+
+        val repository = DictionaryRepository(file.absolutePath)
+        val trainer = LearnWordsTrainer(repository)
+
+        val question = trainer.getNextQuestion()
+        assertNull(question)
+    }
+
+    @Test
+    fun `test checkAnswer() with true`() {
+        val path = "src/test/resources/4_words_of_7.txt"
+        val repository = DictionaryRepository(path)
+        val trainer = LearnWordsTrainer(repository)
+
+        val question = trainer.getNextQuestion()
+        assertNotNull(question)
+
+        val correctIndex = question.correctOptionIndex
+        val result = trainer.checkAnswer(correctIndex)
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun `test checkAnswer() with false`() {
+        val path = "src/test/resources/4_words_of_7.txt"
+        val repository = DictionaryRepository(path)
+        val trainer = LearnWordsTrainer(repository)
+
+        val question = trainer.getNextQuestion()
+        assertNotNull(question)
+
+        val result = trainer.checkAnswer(-1)
+
+        assertFalse(result)
+    }
+
+    @Test
+    fun `test resetProgress() with 2 words in dictionary`() {
+        val file = createTempDictionaryFile(
+            """
+            a|а|2
+            b|б|3
+            """
+        )
+
+        val repository = DictionaryRepository(file.absolutePath)
+        val trainer = LearnWordsTrainer(repository)
+
+        trainer.resetProgress()
+
+        val after = repository.load()
+        assertEquals(0, after[0].correctAnswersCount)
+        assertEquals(0, after[1].correctAnswersCount)
+    }
+}
